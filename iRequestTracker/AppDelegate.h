7b29e8ae3a18ea4dcea55532410dcf5c629daa36
Antonio Haley <antonio.haley@gmail.com>
2012-12-02 11:28:57 -0500

    Initial Commit

---
+++ iRequestTracker/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  iRequestTracker
+//
+//  Created by Antonio Haley on 12/2/12.
+//  Copyright (c) 2012 Antonio Haley. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ iRequestTracker/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  iRequestTracker
+//
+//  Created by Antonio Haley on 12/2/12.
+//  Copyright (c) 2012 Antonio Haley. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
